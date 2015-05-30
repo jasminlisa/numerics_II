3dff874f2d11e3572fe996b2cb9b4ef2ad35de8c
jmaces <jmaces@users.noreply.github.com>
2015-05-30 11:23:55 +0200

    Update glplot.h

--- PA5/Part1/glplot.h
+++ PA5/Part1/glplot.h
@@ -2,56 +2,56 @@
 #define GLPLOT_H
 
 /*
-Draws a set of 2-dimensional data as lines on an OpenGl canvas
-INPUTS:	x		-	the x coordinates as an array
-		y		-	the y coordinates as an array
-		size	-	the length of the data arrays (x and y need to have equal length)
-		xmin	-	the mininal x coordinate to be drawn
-		xmax	-	the maximal x coordinate to be drawn
-		ymin	-	the minimal y coordinate to be drawn
-		ymax	-	the maximal y coordinate to be drawn
-		xpos	-	on-screen opengl x coordinate of the lower left corner of the canvas
-		ypos 	- 	on-screen opengl y coordinate of the lower left corner of the canvas
-		width	-	on-screen opengl width of the canvas
-		height	-	on-screen opengl height of the canvas
-
-OUTPUT: none
-*/
+ * Draws a set of 2-dimensional data as lines on an OpenGl canvas
+ * INPUTS:	@param x	-	the x coordinates as an array
+ *		@param y	-	the y coordinates as an array
+ *		@param size	-	the length of the data arrays (x and y need to have equal length)
+ *		@param xmin	-	the mininal x coordinate to be drawn
+ *		@param xmax	-	the maximal x coordinate to be drawn
+ *		@param ymin	-	the minimal y coordinate to be drawn
+ *		@param ymax	-	the maximal y coordinate to be drawn
+ *		@param xpos	-	on-screen opengl x coordinate of the lower left corner of the canvas
+ *		@param ypos 	- 	on-screen opengl y coordinate of the lower left corner of the canvas
+ *		@param width	-	on-screen opengl width of the canvas
+ *		@param height	-	on-screen opengl height of the canvas
+ *
+ * OUTPUT: 	@return 	-	none
+ */
 void plotArray (double* x, double* y, int size, double xmin, double xmax, double ymin, double ymax, double xpos, double ypos, double width, double height);
 
 /*
-Draws a set of 2-dimensional data as points on an OpenGl canvas
-INPUTS:	x		-	the x coordinates as an array
-		y		-	the y coordinates as an array
-		size	-	the length of the data arrays (x and y need to have equal length)
-		xmin	-	the mininal x coordinate to be drawn
-		xmax	-	the maximal x coordinate to be drawn
-		ymin	-	the minimal y coordinate to be drawn
-		ymax	-	the maximal y coordinate to be drawn
-		xpos	-	on-screen opengl x coordinate of the lower left corner of the canvas
-		ypos 	- 	on-screen opengl y coordinate of the lower left corner of the canvas
-		width	-	on-screen opengl width of the canvas
-		height	-	on-screen opengl height of the canvas
-
-OUTPUT: none
-*/
+ * Draws a set of 2-dimensional data as points on an OpenGl canvas
+ * INPUTS:	@param x	-	the x coordinates as an array
+ *		@param y	-	the y coordinates as an array
+ *		@param size	-	the length of the data arrays (x and y need to have equal length)
+ *		@param xmin	-	the mininal x coordinate to be drawn
+ *		@param xmax	-	the maximal x coordinate to be drawn
+ *		@param ymin	-	the minimal y coordinate to be drawn
+ *		@param ymax	-	the maximal y coordinate to be drawn
+ *		@param xpos	-	on-screen opengl x coordinate of the lower left corner of the canvas
+ *		@param ypos 	- 	on-screen opengl y coordinate of the lower left corner of the canvas
+ *		@param width	-	on-screen opengl width of the canvas
+ *		@param height	-	on-screen opengl height of the canvas
+ *
+ * OUTPUT: 	@return		-	none
+ */
 void plotArrayPoints (double* x, double* y, int size, double xmin, double xmax, double ymin, double ymax, double xpos, double ypos, double width, double height);
 
 /*
-Draws a the graph of a function on an OpenGl canvas
-INPUTS:	f		-	handle for the function to be drawn
-		xmin	-	the mininal x coordinate to be drawn
-		xmax	-	the maximal x coordinate to be drawn
-		ymin	-	the minimal y coordinate to be drawn
-		ymax	-	the maximal y coordinate to be drawn
-		xpos	-	on-screen opengl x coordinate of the lower left corner of the canvas
-		ypos 	- 	on-screen opengl y coordinate of the lower left corner of the canvas
-		width	-	on-screen opengl width of the canvas
-		height	-	on-screen opengl height of the canvas
-		stepsize-	distance of the evaluation points (linear interpoltation will be used in between)
-
-OUTPUT: none
-*/
+ * Draws a the graph of a function on an OpenGl canvas
+ * INPUTS:	@param f	-	handle for the function to be drawn
+ *		@param xmin	-	the mininal x coordinate to be drawn
+ *		@param xmax	-	the maximal x coordinate to be drawn
+ *		@param ymin	-	the minimal y coordinate to be drawn
+ *		@param ymax	-	the maximal y coordinate to be drawn
+ *		@param xpos	-	on-screen opengl x coordinate of the lower left corner of the canvas
+ *		@param ypos 	- 	on-screen opengl y coordinate of the lower left corner of the canvas
+ *		@param width	-	on-screen opengl width of the canvas
+ *		@param height	-	on-screen opengl height of the canvas
+ *		@param stepsize	-	distance of the evaluation points (linear interpoltation will be used in between)
+ *
+ * OUTPUT: 	@return 	-	none
+ */
 void plotFunction (double (*f)(double), double xmin, double xmax, double ymin, double ymax, double xpos, double ypos, double width, double height, double stepsize);
 
 #endif /* GLFONT_H */
