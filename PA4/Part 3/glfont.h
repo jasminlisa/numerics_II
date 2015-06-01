c7d50c900d1c7fd780d79fb8c2dde956218c2d9c
j@math <jmaces@noreply.github.com>
2015-06-01 13:43:05 +0200

    remove double folders

--- PA4/Part 3/glfont.h
+++
@@ -1,9 +0,0 @@
-#ifndef GLFONT_H
-#define GLFONT_H
-
-void buildFont();
-void deleteFont();
-void printGLs(char *text);
-void printGLd(double x, float xCoord, float yCoord);
-
-#endif /* GLFONT_H */
