4d66f26f4c5f798e5dc2faecaec1ba9596b09a3e
j@pool <jmaces@users.noreply.github.com>
2015-06-02 11:22:10 +0200

    init local

---
+++ PA4/Part3/glfont.h
@@ -0,0 +1,9 @@
+#ifndef GLFONT_H
+#define GLFONT_H
+
+void buildFont();
+void deleteFont();
+void printGLs(char *text);
+void printGLd(double x, float xCoord, float yCoord);
+
+#endif /* GLFONT_H */
